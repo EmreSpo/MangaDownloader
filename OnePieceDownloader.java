568e097f73861de5acd6fc6f0836c32794ad80d0
EmreSpo <emre.salihpasaogluTR@gmail.com>
2024-12-11 18:23:43 +0300

    Add files via upload

---
+++ OnePieceDownloader.java
@@ -0,0 +1,17 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URL;
+import java.util.Scanner;
+
+public class OnePieceDownloader {
+    public static void main(String[] args) {
+
+    }
+}
